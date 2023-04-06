a1387297460364e15f53020243c0a3835fb49ed8
Tshego <219001235@mycput.ac.za>
2023-04-06 21:37:05 +0200

    classes modified

--- src/main/java/za/ac/cput/factory/PropertyFactory.java
+++ src/main/java/za/ac/cput/factory/PropertyFactory.java
@@ -1,5 +1,22 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.domain.Address;
+import za.ac.cput.domain.Property;
+import za.ac.cput.domain.PropertyType;
+import za.ac.cput.util.Helper;
+
 public class PropertyFactory {
+    public static Property createProperty(String propertyID, PropertyType propertyType, Address address,
+                                          double rent, int bedsNum, int bathsNum, boolean garageAvailable) {
+        return null;
+    }
+
+    String propertyID = Helper.generateId();
+
+    Property property = new Property.Builder().setPropertyID(propertyID)
+            .setPropertyType(new PropertyType())
+            .setAddress(new Address())
+            .build();
+
 
 }
