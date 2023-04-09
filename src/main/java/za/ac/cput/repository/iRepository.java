d35bd8ef16b84b80d7eabe7dc9201045a26e59b7
Sibusiso Dwayi <sibusisodwayi03@gmail.com>
2023-04-09 13:34:01 +0200

    Contact and Complaint classes added.

--- src/main/java/za/ac/cput/repository/iRepository.java
+++ src/main/java/za/ac/cput/repository/iRepository.java
@@ -1,4 +1,8 @@
 package za.ac.cput.repository;
 
-public interface iRepository {
+public interface iRepository<T,ID> {
+    T create(T t);
+    T read(ID id);
+    T update(T t);
+    boolean delete( ID id);
 }
