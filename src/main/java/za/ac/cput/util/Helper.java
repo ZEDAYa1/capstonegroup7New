2fd022d491bec9f8b9a21b73c3649771e0b1b2ba
ZEDAYa1 <220097429@mycput.ac.za>
2023-04-09 14:55:07 +0200

    Factory, Repository, and Util classes created

--- src/main/java/za/ac/cput/util/Helper.java
+++ src/main/java/za/ac/cput/util/Helper.java
@@ -8,11 +8,19 @@ package za.ac.cput.util;
 
 import org.apache.commons.validator.routines.EmailValidator;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Helper {
     public static boolean isNullOrEmpty(String s){
         if(s == null || s.isEmpty() ||s.equalsIgnoreCase("null")){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isDateNull(Date date){
+        if(date == null ){
             return true;
         }
         return false;
