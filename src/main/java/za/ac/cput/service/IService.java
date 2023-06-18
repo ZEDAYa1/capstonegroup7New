25402e139892b9797a24809b21f9c49ddef5a83c
Sibusiso Dwayi <sibusisodwayi03@gmail.com>
2023-06-18 22:59:56 +0200

    IService updated

--- src/main/java/za/ac/cput/service/IService.java
+++ src/main/java/za/ac/cput/service/IService.java
@@ -1,4 +1,11 @@
 package za.ac.cput.service;
 
-public interface IService {
+import java.util.Set;
+
+public interface IService<T,ID >{
+    T save (T t);
+    T read (ID id);
+    boolean delete(ID id);
+    Set<T> findAll();
+
 }
