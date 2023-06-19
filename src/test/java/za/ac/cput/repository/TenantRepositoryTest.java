cd6f590c6b488bab4582cb4eb669031fada62ea2
tshegomolefe <219001235@mycput.ac.za>
2023-06-19 20:20:46 +0200

    All Tenant-related classed modified

--- src/test/java/za/ac/cput/repository/TenantRepositoryTest.java
+++ src/test/java/za/ac/cput/repository/TenantRepositoryTest.java
@@ -1,6 +1,6 @@
 /*
- * ClientRepositoryTest.java
- * This is the test for the client repository
+ * TenantRepositoryTest.java
+ * This is the test for the tenant repository
  * Author: Tshegofatso Molefe {219001235}
  * Date: 8 April 2023
  * */
@@ -9,25 +9,25 @@ package za.ac.cput.repository;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import za.ac.cput.domain.Client;
-import za.ac.cput.factory.ClientFactory;
+import za.ac.cput.domain.Tenant;
+import za.ac.cput.factory.TenantFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
-class ClientRepositoryTest {
-    private static ClientRepository repository = ClientRepository.getRepository();
-    private static Client client = ClientFactory.createClient("Tshego", "Molefe", "tshego@email.com", "0213568978");
+class TenantRepositoryTest {
+    private static TenantRepository repository = TenantRepository.getRepository();
+    private static Tenant tenant = TenantFactory.createTenant("Tshego", "Molefe", "tshego@email.com", "0213568978");
     @Test
     void a_create() {
-        Client created = repository.create(client);
-        assertNotNull(client.getClientID(), created.getClientID());
+        Tenant created = repository.create(tenant);
+        assertNotNull(tenant.getTenantID(), created.getTenantID());
         System.out.println(created);
     }
 
     @Test
     void b_read() {
-        Client read = repository.read(client.getClientID());
+        Tenant read = repository.read(tenant.getTenantID());
         assertNotNull(read);
         System.out.println(read);
 
@@ -35,8 +35,8 @@ class ClientRepositoryTest {
 
     @Test
     void c_update() {
-        Client updated = new Client.Builder().copy(client).setEmail("Name@email.com")
-                .setMobileNumber("0812345678")
+        Tenant updated = new Tenant.Builder().copy(tenant).setEmail("Name@email.com")
+                .setContactNumber("0812345678")
                 .build();
         assertNotNull(repository.update(updated));
         System.out.println("Updates :" + updated);
@@ -44,7 +44,7 @@ class ClientRepositoryTest {
 
     @Test
     void e_delete() {
-        boolean success = repository.delete(client.getClientID());
+        boolean success = repository.delete(tenant.getTenantID());
         assertTrue(success);
         System.out.println("Deleted :" + success);
     }
