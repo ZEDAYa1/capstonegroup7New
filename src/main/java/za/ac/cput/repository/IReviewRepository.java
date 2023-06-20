e7b635416fb8ce1f11cb41b5ab6f9fb30722cc1c
Sibusiso Dwayi <sibusisodwayi03@gmail.com>
2023-06-21 00:19:27 +0200

    ReviewRepository updated.

--- src/main/java/za/ac/cput/repository/IReviewRepository.java
+++ src/main/java/za/ac/cput/repository/IReviewRepository.java
@@ -8,6 +8,6 @@ package za.ac.cput.repository;
 import za.ac.cput.domain.Review;
 import java.util.Set;
 
-public interface IReviewRepository extends iRepository<Review, String> {
+public interface IReviewRepository extends IRepository<Review, String> {
     public Set<Review> getAll();
 }
