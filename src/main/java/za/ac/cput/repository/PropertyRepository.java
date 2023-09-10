14417b08444e0c64f9f05e1aad3ce230182a7af1
Tshego-Molefe <80263676+Tshego-Molefe@users.noreply.github.com>
2023-09-10 16:23:00 +0200

    Update PropertyRepository.java

--- src/main/java/za/ac/cput/repository/PropertyRepository.java
+++ src/main/java/za/ac/cput/repository/PropertyRepository.java
@@ -1,64 +1,14 @@
 package za.ac.cput.repository;
 
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.Property;
 
 import java.util.HashSet;
 import java.util.Set;
 
-public class PropertyRepository implements IPropertyRepository {
-    private static PropertyRepository repository = null;
-    private Set<Property> propertyDB = null;
+@Repository
+public interface PropertyRepository extends JpaRepository<Property, String> {
 
-    private PropertyRepository(){
-        propertyDB = new HashSet<Property>();
-    }
-
-    public static PropertyRepository getRepository(){
-        if (repository == null) {
-            repository = new PropertyRepository();
-        }
-        return repository;
-    }
-    @Override
-    public Property create(Property property) {
-        boolean success = propertyDB.add(property);
-        if(!success)
-            return null;
-        return property;
-    }
-
-    @Override
-    public Property read(String propertyID) {
-        Property property = propertyDB.stream()
-                .filter(c -> c.getPropertyID().equals(propertyID))
-                .findAny()
-                .orElse(null);
-        return property;
-    }
-
-    @Override
-    public Property update(Property property) {
-        Property oldProperty = read(property.getPropertyID());
-        if( oldProperty != null){
-            propertyDB.remove(oldProperty);
-            propertyDB.add(property);
-            return property;
-        }
-        return null;
-    }
-
-    @Override
-    public boolean delete(String propertyID) {
-        Property propertyToDelete = read(propertyID);
-        if(propertyToDelete == null)
-            return false;
-        propertyDB.remove(propertyToDelete);
-        return true;
-    }
-
-    @Override
-    public Set<Property> getAll() {
-        return propertyDB;
-    }
 }
