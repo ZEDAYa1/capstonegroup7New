8d7f33987c8346c6fa19bda42f989b8f02591669
ZEDAYa1 <220097429@mycput.ac.za>
2023-09-20 22:40:35 +0200

    Updated Lease and Booking repository.

--- src/main/java/za/ac/cput/repository/ILeaseRepository.java
+++ src/main/java/za/ac/cput/repository/ILeaseRepository.java
@@ -1,8 +1,10 @@
 package za.ac.cput.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.Lease;
 
+@Repository
 public interface ILeaseRepository extends JpaRepository<Lease, String> {
 
 }
