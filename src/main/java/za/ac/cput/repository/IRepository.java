6021be724d11f9957c89f8aded05fff34763d130
Tshegofatso Molefe <molefet2@legal.regn.net>
2023-10-22 15:04:04 +0200

    ServiceImpl amended

--- src/main/java/za/ac/cput/repository/IRepository.java
+++ src/main/java/za/ac/cput/repository/IRepository.java
@@ -2,7 +2,7 @@ package za.ac.cput.repository;
 
 @Deprecated
 public interface IRepository<T,ID>{
-    T create(T t);
+    T save(T t);
     T read(ID id);
     T update(T t);
     boolean delete(ID id);
